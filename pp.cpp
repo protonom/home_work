35c0701e2a0614a2bfc0b9e1085c40f965a1fab2
gotonom <gotonom@gmail.com>
2024-12-21 13:39:04 +0300

    third_branch

--- pp.cpp
+++ pp.cpp
@@ -3,10 +3,15 @@
 
 void S_taingle(int a, int h) 
 {
-    std::cout << "S=" << (a * h) / 2;
+    std::cout << "S=" << (a * h) / 2 << "\n";
+}
+void circle_S(double r) {
+    std::cout << "S=" << 3.14 * (r*r);
 }
 int main()
 {
     int a = 5, h = 4;
+    double r = 15.23;
     S_taingle(a, h);
+    circle_S(r);
 }
